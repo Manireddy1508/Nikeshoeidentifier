878cd3d9aa005e5b031957e62735f83e27df1304
manichandrareddy <manichandrareddybethi37@gmail.com>
2025-02-12 21:47:07 -0500

    Updated model.py before pulling

--- app/model.py
+++ app/model.py
@@ -1,33 +1,19 @@
-import numpy as np
-from tensorflow.keras.models import load_model
-from PIL import Image
+import tensorflow as tf
 import os
 
-# Correct model path
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))  # Get current directory
-MODEL_PATH = os.path.join(BASE_DIR, "../Model/nike_shoe_classifier.h5")  # Go one level up
+MODEL_PATH = "app/nike_shoe_classifier.h5"
 
-# Load model
-model = load_model(MODEL_PATH)
+def load_model_and_predict(image):
+    """Load model and make a prediction"""
+    model = tf.keras.models.load_model(MODEL_PATH)
+    
+    # Preprocess image
+    img = image.resize((224, 224))
+    img = tf.keras.preprocessing.image.img_to_array(img) / 255.0
+    img = tf.expand_dims(img, axis=0)
 
-def preprocess_image(image: Image.Image):
-    """
-    Preprocess an image for model prediction.
-    - Resize to (224, 224)
-    - Normalize pixel values
-    - Expand dimensions for batch format
-    """
-    image = image.resize((224, 224))  # Resize to model input size
-    image = np.array(image) / 255.0   # Normalize pixel values
-    image = np.expand_dims(image, axis=0)  # Add batch dimension
-    return image
+    # Get prediction
+    prediction = model.predict(img)
+    
+    return "Nike" if prediction[0][0] < 0.5 else "Other"
 
-def load_model_and_predict(image: Image.Image):
-    """
-    Process the image and run model inference.
-    Returns "Nike" if prediction is < 0.5, otherwise "Other".
-    """
-    image = preprocess_image(image)
-    prediction = model.predict(image)
-
-    return "Nike" if prediction[0] < 0.5 else "Other"
